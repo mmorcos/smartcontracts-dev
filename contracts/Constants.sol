2d4b923638a86bae9492f0a16effad6c40fcf648
mmorcos <m.morcos94@rutgers.edu>
2022-03-29 21:20:52 -0400

    Create Constants.sol

---
+++ contracts/Constants.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.8.7;
+
+// Gas Difference 
+contract Constant {
+    address public constant MY_ADDRESS = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
+
+contract Var {
+    address public  MY_ADDRESS = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
